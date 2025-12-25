9da1cd61f236f23cc25b94ae7bb9cee55a34bde8
clz123456clz <clz123456clz@sjtu.edu.cn>
2025-12-25 05:43:10 +0000

    Finish difussion building blocks

---
+++ sd/clip.py
@@ -0,0 +1,84 @@
+import torch 
+from torch import nn
+from torch.nn import functional as F
+from attention import SelfAttention
+
+class CLIPEmbedding (nn.Module):
+    def __init__(self, n_vocab: int, n_embd: int, n_token: int):
+        super().__init__()
+
+        self.token_embedding = nn.Embedding(n_vocab, n_embd)
+        self.position_embedding = nn.Parameter(torch.zeros(n_token, n_embd))
+
+    def forward(self, tokens):
+        # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+        x = self.token_embedding(tokens)
+        # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+        x += self.position_embedding
+
+class CLIPLayer(nn.Module):
+    def __init__(self, n_head: int, n_embd: int):
+        super().__init__()
+        # Pre-attention norm
+        self.layernorm_1 = nn.LayerNorm(n_embd)
+        # Self attention
+        self.attention = SelfAttention(n_head, n_embd)
+        # Pre-FNN norm
+        self.layernorm_2 = nn.LayerNorm(n_embd)
+        # Feedforward layer
+        self.linear_1 = nn.Linear(n_embd, 4 * n_embd)
+        self.linear_2 = nn.Linear(4 * n_embd, n_embd)
+    
+    def forward(self, x):
+        # (Batch_Size, Seq_Len, Dim)
+        residue = x
+
+        x = self.layernorm_1(x)
+        x = self.attention(x)
+        x += residue
+
+        residue = x
+        x = self.layernorm_2(x)
+        x = self.linear_1(x)
+        x = x * torch.sigmoid(1.702 * x) # QuickGELU activation function
+        x = self.linear_2(x)
+        x += residue
+
+        return x
+
+
+
+
+
+
+
+
+
+class CLIP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embedding = CLIPEmbedding(49408, 768, 77) # (vocab_size, embedding_dim(hidden_size), max_seq_len)
+
+        self.layers = nn.ModuleList([
+            CLIPLayer(12, 768) for i in range(12) # (n_heads, hidden_size)
+        ])
+
+        self.layernorm = nn.LayerNorm(768)
+
+    def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
+        tokens = tokens.type(torch.long)
+
+        # (Batch_size, Seq_len) -> (Batch_size, Seq_len, hidden_dim)
+        state = self.embedding(tokens)
+
+        # Apply encoder layers similar to the Transformer's encoder.
+        for layer in self.layers: 
+            # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
+            state = layer(state)
+        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
+        output = self.layernorm(state)
+        
+        return output
+
+
+
